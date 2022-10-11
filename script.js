531934c6563a0f53451de7c564b6bb7b00cb1cf0
Rodrigo Botelho <98762052+rodrigocbotelho@users.noreply.github.com>
2022-10-11 16:44:11 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,56 +1,55 @@
-
 function imcCal() {
-  var altura = document.getElementById("altura").value, 
+  let altura = document.getElementById("altura").value, 
       peso = document.getElementById("peso").value;
   
-  var imc = Number(peso / (altura ** 2)).toFixed(1);
-  
+  try{
+    var imc = calculo(peso, altura);
+  } catch(e){
+    alert(e);
+    return;
+  };
+ 
+  let status = null;
   if (imc < 17) {
-    
-    var status = 'Muito abaixo do peso';
-    
+    status = 'Muito abaixo do peso';
   } 
   
   else if (imc >=17 && imc < 18.5){
-    
      status = 'Abaixo do peso';
-    
   }
   
   else if (imc >= 18.5 && imc < 25){
-    
     status = 'Peso ideal';
-    
   }
   else if (imc >= 25 && imc < 30){
-    
     status = 'Sobrepeso';
-    
   }
   else if (imc >= 30 && imc < 35) {
-    
     status = 'Obesidade I';
-    
   }
   else if (imc >= 35 && imc < 40) {
-    
     status = 'Obesidade II';
-    
   }
   else if (imc > 40) {
-    
     status = 'Obesidade III';
-    
   }
   else{
-    
-    imc = 0
     status = 0;
-    
-  }
+  };
   
-  resul = document.getElementById("result");
-  
-  resul.innerHTML = '<span class="info">IMC:</span> '+ imc + '</br>' + '<span class="info">Status:</span> ' + status;
-  
-}
+  let msg = '<span class="info">IMC:</span> '+ imc + '</br>' + '<span class="info">Status:</span> ' + status;
+  mostrar(msg);
+};
+
+function calculo(peso, altura){
+  let calc = Number(peso / (altura ** 2)).toFixed(1);
+  if(isNaN(calc)){
+    throw new Error('VALORES INVALIDOS!');
+   };
+  return calc;
+};
+
+function mostrar(mensagem){
+    let resul = document.getElementById("result");
+    resul.innerHTML = mensagem;
+};
