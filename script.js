3f93f022e32212a883fe46a1877b0fcb6e2a9d02
Rodrigo Botelho <98762052+rodrigocbotelho@users.noreply.github.com>
2023-01-15 17:04:26 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,55 +1,63 @@
 function imcCal() {
-  let altura = document.getElementById("altura").value, 
-      peso = document.getElementById("peso").value;
-  
-  try{
+  let altura = document.getElementById("altura").value,
+    peso = document.getElementById("peso").value;
+
+  try {
     var imc = calculo(peso, altura);
-  } catch(e){
+  } catch (e) {
     alert(e);
     return;
-  };
- 
+  }
+
   let status = null;
   if (imc < 17) {
-    status = 'Muito abaixo do peso';
-  } 
-  
-  else if (imc >=17 && imc < 18.5){
-     status = 'Abaixo do peso';
-  }
-  
-  else if (imc >= 18.5 && imc < 25){
-    status = 'Peso ideal';
-  }
-  else if (imc >= 25 && imc < 30){
-    status = 'Sobrepeso';
-  }
-  else if (imc >= 30 && imc < 35) {
-    status = 'Obesidade I';
-  }
-  else if (imc >= 35 && imc < 40) {
-    status = 'Obesidade II';
-  }
-  else if (imc > 40) {
-    status = 'Obesidade III';
-  }
-  else{
+    status = "Muito abaixo do peso";
+  } else if (imc >= 17 && imc < 18.5) {
+    status = "Abaixo do peso";
+  } else if (imc >= 18.5 && imc < 25) {
+    status = "Peso ideal";
+  } else if (imc >= 25 && imc < 30) {
+    status = "Sobrepeso";
+  } else if (imc >= 30 && imc < 35) {
+    status = "Obesidade I";
+  } else if (imc >= 35 && imc < 40) {
+    status = "Obesidade II";
+  } else if (imc > 40) {
+    status = "Obesidade III";
+  } else {
     status = 0;
-  };
-  
-  let msg = '<span class="info">IMC:</span> '+ imc + '</br>' + '<span class="info">Status:</span> ' + status;
+  }
+
+  let msg =
+    '<span class="info">IMC:</span> ' +
+    imc +
+    "</br>" +
+    '<span class="info">Status:</span> ' +
+    status;
   mostrar(msg);
-};
+}
 
-function calculo(peso, altura){
-  let calc = Number(peso / (altura ** 2)).toFixed(1);
-  if(isNaN(calc)){
-    throw new Error('VALORES INVALIDOS!');
-   };
+function calculo(peso, altura) {
+  let calc = Number(peso / altura ** 2).toFixed(1);
+  if (isNaN(calc)) {
+    throw new Error("VALORES INVALIDOS!");
+  }
   return calc;
-};
+}
 
-function mostrar(mensagem){
-    let resul = document.getElementById("result");
-    resul.innerHTML = mensagem;
-};
+function mostrar(mensagem) {
+  let resul = document.getElementById("result");
+  resul.innerHTML = mensagem;
+}
+
+peso.addEventListener("keyup", function (e) {
+  if (e.keyCode === 13) {
+    altura.focus();
+  }
+});
+
+altura.addEventListener("keyup", function (e) {
+  if (e.keyCode === 13) {
+    imcCal();
+  }
+});
